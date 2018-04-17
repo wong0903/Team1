47e1f0e05b4dc5580c6307c610c57cb509c880f4
abcnaiteel <ryo_tianleong@hotmail.com>
2018-04-18 01:08:44 +0800

    adding mockito

---
+++ visitSG2/app/src/test/java/control/UserManagerTest.java
@@ -0,0 +1,25 @@
+package control;
+
+import org.junit.Test;
+
+import Database.AppDatabase;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nigelleong on 18/4/18.
+ */
+public class UserManagerTest {
+    @Test
+    public void signUp() throws Exception {
+        AppDatabase db;
+        String loginID;
+        String password;
+
+        loginID = "";
+        password = "abc123";
+
+
+    }
+
+}
